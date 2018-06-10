d633301b17c45d7bf22e5bcfcd2ece871f6125e3
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-06-10 12:11:33 +0200

    initial commit

---
+++ client/modules/Notes/Notes.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './Notes.css';
+
+class Notes extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Notes.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notes);
---
+++ server/controllers/notes.controller.js
@@ -0,0 +1,5 @@
+import Notes from '../models/notes';
+
+export function getSomething(req, res) {
+  return res.status(200).end();
+}
